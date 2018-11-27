a0df5cd4876ac4a327d2d9cbf3d42983628b659a
phillipf <phillip-f@live.com>
2018-11-28 09:18:36 +1100

    emacs test

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -95,9 +95,9 @@ object main extends App {
   //val filename = new File(args(0))
   //val output = new File(args(1))
   //multiple payloads
-  val filename = new File("C:/Users/farrelp1/Documents/payloadParser/src/test/data/jsonMulti1.json")
+  //val filename = new File("C:/Users/farrelp1/Documents/payloadParser/src/test/data/jsonMulti1.json")
   //multiple payloads
-  //val filename = new File("C:/Users/farrelp1/Documents/DigitalMetering/data/Nb-IoT Payloads_imei-863703032742533_19-10-2018/cww-2018-10-18-16-08-54.json")
+  val filename = new File("C:/Users/farrelp1/Documents/DigitalMetering/data/Nb-IoT Payloads_imei-863703032742533_19-10-2018/cww-2018-10-18-16-08-54.json")
   //updates payloads
   //val filename = new File("C:/Users/farrelp1/Documents/DigitalMetering/data/Nb-IoT Payloads_imei-863703032742533_19-10-2018/cww-2018-10-18-16-07-46.json")
 
